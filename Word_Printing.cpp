58362041687e7ee8f76c9b3eb44c55ef83d3de21
Yashvesh20 <yashvesh20@gmail.com>
2025-07-27 18:30:35 +0530

    Create Word_Printing.cpp

---
+++ Word_Printing.cpp
@@ -0,0 +1,17 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    for(i=0;i<5;i++)
+    {
+       cout <<"Yashvesh"<<endl;
+    }
+}
+
+Output:
+Yashvesh
+Yashvesh
+Yashvesh
+Yashvesh
+Yashvesh
