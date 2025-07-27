a85f2b2ff755bd5afcfcbb6e1d5c7924f39d8cd5
Yashvesh20 <yashvesh20@gmail.com>
2025-07-27 18:27:51 +0530

    Create Even_numbera.cpp

---
+++ Even_numbera.cpp
@@ -0,0 +1,23 @@
+Yashvesh Singh 24070123138
+#include<iostream>
+using namespace std;
+int main(){
+   int i;
+    for(i=0;i<11;i++)
+    {
+        if (i%2==0){
+            cout <<i<<endl;
+        }
+        else{
+            continue;
+        }
+    }
+}
+
+Output:
+0
+2
+4
+6
+8
+10
